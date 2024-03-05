4e5d19d44f918b70e057c9b72b8946f5ad97571a
ShannonRoseveare <131300342+ShannonRoseveare@users.noreply.github.com>
2024-03-05 15:00:09 +0000

    Update Dash - FINAL.py

--- Dash - FINAL.py
+++ Dash - FINAL.py
@@ -1,159 +1,98 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
+# Import required libraries
 import pandas as pd
-import plotly.graph_objs as go
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
 import plotly.express as px
 
-# Load the data using pandas
-data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/historical_automobile_sales.csv')
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
 
-# Initialize the Dash app
+# Create a dash application
 app = dash.Dash(__name__)
-app.config.suppress_callback_exceptions = True
-# Set the title of the dashboard
-#app.title = "Automobile Statistics Dashboard"
+dropdown_options=[{'label': 'All Sites', 'value': 'ALL'}
+        ,{'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'}
+        ,{'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'}
+        ,{'label': 'KSC LC-39A', 'value': 'KSC LC-39A'}
+        ,{'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'}]
 
-#---------------------------------------------------------------------------------
-# Create the dropdown menu options
-dropdown_options = [
-    {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
-    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
-]
-# List of years 
-year_list = [i for i in range(1980, 2024, 1)]
-#---------------------------------------------------------------------------------------
-# Create the layout of the app
-app.layout = html.Div([
-    #TASK 2.1 Add title to the dashboard
-    html.H1("Automobile Sales Statistics Dashboard"
-    ,
-            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 24}),#May include style for title
-    html.Div([#TASK 2.2: Add two dropdown menus
-        html.Label("Select Statistics:"),
-        dcc.Dropdown(
-            id='dropdown-statistics',
-            options=dropdown_options,
-            value='Select Statistics',
-            placeholder='Select a report type'
-        )
-    ]),
-    html.Div(dcc.Dropdown(
-            id='select-year',
-            options=[{'label': i, 'value': i} for i in year_list],
-            placeholder='2025',
-        )),
-    html.Div([#TASK 2.3: Add a division for output display
-    html.Div(id='output-container', className='chart-grid'
-    , style={'align':'flex'}
-    )])
-])
-#TASK 2.4: Creating Callbacks
-# Define the callback function to update the input container based on the selected statistics
-@app.callback(
-    Output(component_id='select-year', component_property='disabled'),
-    Input(component_id='dropdown-statistics',component_property='value'))
+# Create an app layout
+app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
+                                        style={'textAlign': 'center', 'color': '#503D36',
+                                               'font-size': 40}),
+                                # TASK 1: Add a dropdown list to enable Launch Site selection
+                                # The default select value is for ALL sites
+                                  dcc.Dropdown(id='site-dropdown',
+                                               options=dropdown_options,
+                                               value='ALL',
+                                               placeholder="place holder here",
+                                               searchable=True
+                                               ),
+                                html.Br(),
 
-def update_input_container(value):
-    if value == 'Yearly Statistics': 
-        return False
-    else: 
-        return True
-#Callback for plotting
-# Define the callback function to update the input container based on the selected statistics
-@app.callback(
-    Output(component_id='output-container', component_property='children'),
-     [Input(component_id='select-year', component_property='value'),
-     Input(component_id='dropdown-statistics', component_property='value')])
+                                # TASK 2: Add a pie chart to show the total successful launches count for all sites
+                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
+                                html.Div(dcc.Graph(id='success-pie-chart')),
+                                html.Br(),
 
-def update_output_container(input_year,selected_statistic):
-    print(selected_statistic,input_year)
-    if selected_statistic == 'Recession Period Statistics':
-        # Filter the data for recession periods
-        recession_data = data[data['Recession'] == 1]
-        
-#TASK 2.5: Create and display graphs for Recession Report Statistics
+                                html.P("Payload range (Kg):"),
+                                # TASK 3: Add a slider to select payload range
+                                  dcc.RangeSlider(id='payload-slider',
+                                                 min=0, 
+                                                 max=10000, 
+                                                 step=1000,
+                                                 marks={0: '0', 1000: '1000', 2000: '2000', 3000:'3000', 4000:'4000', 5000: '5000', 6000:'6000',7000:'7000',8000:'8000', 9000:'9000',10000:'10000'},
+                                                 value=[min_payload, max_payload]
+                                               ),
 
-#Plot 1 Automobile sales fluctuate over Recession Period (year wise)
-        # use groupby to create relevant data for plotting
-        yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
-        R_chart1 = dcc.Graph(
-            figure=px.line(yearly_rec, 
-                x='Year',
-                y='Automobile_Sales',
-                title="Average Automobile Sales fluctuation over Recession Period"))
+                                # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+                                html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+                                ])
 
-#Plot 2 Calculate the average number of vehicles sold by vehicle type       
-        # use groupby to create relevant data for plotting
-        average_sales = recession_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()                           
-        R_chart2  = dcc.Graph(figure=px.bar(average_sales, x= 'Vehicle_Type',y='Automobile_Sales',title='Average Sales by Vehicle type'))
-        
-# Plot 3 Pie chart for total expenditure share by vehicle type during recessions
-        # use groupby to create relevant data for plotting
-        exp_rec = recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
-        R_chart3 = dcc.Graph(figure=px.pie(exp_rec,values='Advertising_Expenditure',names='Vehicle_Type',
-                    title = 'Total Advertising Expenditure by Vehicle Type'))
-
-# Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
-        unemp_rec = recession_data.groupby('Vehicle_Type').sum().reset_index()
-        R_chart4 = dcc.Graph(figure=px.bar(unemp_rec, x='unemployment_rate',y='Automobile_Sales',color='Vehicle_Type',
-        title = 'Automobile Sales by Unemployment rate and Vehicle Type'))
-
-        return [
-            html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)]
-            ,style={'align':'flex'}
-            ),
-            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)]
-           ,style={'align':'flex'}
-            )
-            ]
-
-# TASK 2.6: Create and display graphs for Yearly Report Statistics
- # Yearly Statistic Report Plots                             
-    elif (selected_statistic =='Yearly Statistics' and input_year) :
-        yearly_data = data[data['Year'] == input_year]
-   # elif True:
-    #    print(input_year,selected_statistices)                      
-#TASK 2.5: Creating Graphs Yearly data
-                              
-#plot 1 Yearly Automobile sales using line chart for the whole period.
-        yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
-        Y_chart1 = dcc.Graph(figure=px.line(yas,x='Year',y='Automobile_Sales',title=f'Yearly Automobile Sales: {input_year}'))
-            
-# Plot 2 Total Monthly Automobile sales using line chart.
-        mdata = yearly_data.groupby('Month')['Automobile_Sales'].mean().reset_index()
-        Y_chart2 = dcc.Graph(figure=px.line(mdata,x='Month',y='Automobile_Sales',title='Monthly Automobile Sales'))
-
-            # Plot bar chart for average number of vehicles sold during the given year
-        avr_vdata=yearly_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()
-        Y_chart3 = dcc.Graph( figure=px.bar(avr_vdata,x='Vehicle_Type',y='Automobile_Sales',title='Average Vehicles Sold by Vehicle Type in the year {}'.format('Select Year')))
-
-            # Total Advertisement Expenditure for each vehicle using pie chart
-        exp_data=yearly_data.groupby('Vehicle_Type')['Automobile_Sales'].sum().reset_index()
-        Y_chart4 = dcc.Graph(figure=px.pie(exp_data,values='Automobile_Sales',names='Vehicle_Type',title='Total sales by Vehicle Type'))
-
-#TASK 2.6: Returning the graphs for displaying Yearly data
-        return [
-                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)]
-                ,style={'align':'flex'}
-                ),
-                html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)]
-                ,style={'align':'flex'}
-                )
-                ]
-        
+# TASK 2:
+# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Function decorator to specify function input and output
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+def get_pie_chart(entered_site):
+    if entered_site == 'ALL':
+        filtered_df = spacex_df[spacex_df['class']==1]
+        fig = px.pie(filtered_df, values='class', 
+        names='Launch Site', 
+        title='Total success launches by site')
+        return fig
+        print(filtered_df.head())
     else:
-        return None
+        filtered_df2 = spacex_df[spacex_df['Launch Site']==entered_site].groupby('class')['class'].count()
+        fig2 = px.pie(filtered_df2, values = 'class',names ='class', color='class', title = 'Total success launches for site ' + entered_site)
+        return fig2
+        print(filtered_df.head())
+        # return the outcomes piechart for a selected site
+# TASK 4:
+# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(Output(component_id='success-payload-scatter-chart',component_property='figure'),
+              [Input(component_id='site-dropdown', component_property='value'), 
+               Input(component_id='payload-slider', component_property='value')])
 
-# Run the Dash app
+def get_line_chart(entered_site, payload_range):
+    #print(spacex_df.dtypes,payload_range[1].dtypes)
+    filtered_df = spacex_df[spacex_df['Payload Mass (kg)'].between(payload_range[0],payload_range[1])]
+    if entered_site == 'ALL':
+       #filtered_df = spacex_df[spacex_df['Payload Mass (kg)'].between(payload_range[0],payload_range[1])]
+        scatter = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category',
+        title='Correlation between payload and success for all sites')
+        return scatter
+        #print(filtered_df.head())
+    else:
+        data = filtered_df[filtered_df['Launch Site']==entered_site]
+        scatter2 = px.scatter(data, x = 'Payload Mass (kg)',y='class', color='Booster Version Category',title ='Correlation between payload and success for site' + entered_site)
+        return scatter2
+       # print(filtered_df.head())
+
+# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
 
